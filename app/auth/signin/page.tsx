1621bc267f96f992111df779b57e8d1ae1a5316b
Ruihuang Yang <rxy216@case.edu>
2024-06-03 01:38:57 -0400

    migrate

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { Button, Card, Image } from "@nextui-org/react";
+import { DarkModeSwitch } from "@/components/navbar/darkmodeswitch";
+
+const SigninPage: React.FC = () => {
+  const handleSSOLogin = () => {
+    // Implement SSO login logic here
+    console.log("SSO login initiated");
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100 dark:bg-gray-900">
+      <div className="absolute top-4 left-4 flex items-center space-x-2 text-2xl font-bold">
+        <Image
+          src="/favicon.ico"
+          alt="AI4EDU"
+          className="h-10 rounded-lg mr-3"
+        />
+        AI4EDU
+      </div>
+      <div className="absolute top-4 right-4">
+        <DarkModeSwitch />
+      </div>
+      <Card className="w-full max-w-md p-8 shadow-lg rounded-xl">
+        <h1 className="text-3xl font-semibold mb-6 text-center">Sign in</h1>
+        <div className="flex justify-center">
+          <Button
+            onClick={handleSSOLogin}
+            className="px-8 py-3 text-lg"
+            size="lg"
+          >
+            <Image src="/favicon.ico" alt="AI4EDU" className="h-7 rounded-lg" />
+            Sign in with CWRU
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default SigninPage;
