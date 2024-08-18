dd25b43423f80282a128f0227b017e8912ecfaef
Ruihuang Yang <rxy216@case.edu>
2024-08-18 17:42:12 -0400

    add delay to ping

--- components/home/agent-join-modal.tsx
+++ components/home/agent-join-modal.tsx
@@ -1,46 +1,57 @@
-import { Modal, ModalHeader, ModalBody, ModalFooter, ModalContent, Button, Input } from '@nextui-org/react'
-import { useContext, useState } from 'react'
-import { deleteAgent } from '@/api/agent/agent'
-import { WorkspaceContext } from '@/components/layout/layout'
-import { studentJoinWorkspace } from '@/api/workspace/workspace'
-import { MdInfoOutline } from 'react-icons/md'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Cookies from 'js-cookie'
-import { ping } from '@/api/auth/auth'
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalContent,
+  Button,
+  Input,
+} from "@nextui-org/react";
+import { useContext, useState } from "react";
+import { WorkspaceContext } from "@/components/layout/layout";
+import { studentJoinWorkspace } from "@/api/workspace/workspace";
+import { MdInfoOutline } from "react-icons/md";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Cookies from "js-cookie";
+import { ping } from "@/api/auth/auth";
 
 const AgentJoinModal = ({ isOpen, onClose }) => {
-  const { currentWorkspace, setCurrentWorkspace } = useContext(WorkspaceContext)
-  const [workspaceID, setWorkspaceID] = useState('')
-  const [workspacePassword, setWorkspacePassword] = useState('')
+  const { currentWorkspace, setCurrentWorkspace } =
+    useContext(WorkspaceContext);
+  const [workspaceID, setWorkspaceID] = useState("");
+  const [workspacePassword, setWorkspacePassword] = useState("");
 
   const handleCloseModal = () => {
-    setWorkspaceID('')
-    setWorkspacePassword('')
-    onClose()
-  }
+    setWorkspaceID("");
+    setWorkspacePassword("");
+    onClose();
+  };
 
   const onSubmit = () => {
-    const param = { workspace_id: workspaceID, password: workspacePassword }
+    const param = { workspace_id: workspaceID, password: workspacePassword };
     studentJoinWorkspace(param)
       .then((response) => {
-        toast.success('Join successful, please logout and login again to see the changes')
-        handleCloseModal()
-        Cookies.remove('access_token')
-        ping()
-          .then((res) => {
-            // if the refresh token is valid, set the new access token
-            window.location.reload()
-          })
-          .catch((err) => {
-            window.location.href = '/auth/signin'
-          })
-        console.log('response', response)
+        toast.success(
+          "Join successful, please logout and login again to see the changes"
+        );
+        handleCloseModal();
+        Cookies.remove("access_token");
+        setTimeout(() => {
+          ping()
+            .then((res) => {
+              window.location.reload();
+            })
+            .catch((err) => {
+              window.location.href = "/auth/signin";
+            });
+        }, 100);
+        console.log("response", response);
       })
       .catch((error) => {
-        console.error('Error joining workspace:', error)
-      })
-  }
+        console.error("Error joining workspace:", error);
+      });
+  };
 
   return (
     <div>
@@ -52,8 +63,10 @@ const AgentJoinModal = ({ isOpen, onClose }) => {
             <div className="flex items-center gap-2 bg-gray-100 p-3 rounded-lg">
               <MdInfoOutline size={24} />
               <span className="text-black-100 text-xs">
-                To join new workspace, enter <span className="font-semibold">Workspace ID</span> and
-                <span className="font-semibold"> Workspace Password</span> provided from your instructor.
+                To join new workspace, enter{" "}
+                <span className="font-semibold">Workspace ID</span> and
+                <span className="font-semibold"> Workspace Password</span>{" "}
+                provided from your instructor.
               </span>
             </div>
 
@@ -81,14 +94,18 @@ const AgentJoinModal = ({ isOpen, onClose }) => {
             <Button variant="faded" onClick={handleCloseModal}>
               Cancel
             </Button>
-            <Button color="primary" onClick={onSubmit} isDisabled={!workspaceID || !workspacePassword}>
+            <Button
+              color="primary"
+              onClick={onSubmit}
+              isDisabled={!workspaceID || !workspacePassword}
+            >
               Join
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default AgentJoinModal
+export default AgentJoinModal;
