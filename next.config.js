97c24824141fd7ed2e76b0978f1733514edecdb2
Ray Ikome <raycikome@gmail.com>
2024-10-06 10:35:31 -0400

    no console logs in prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    //no console logs in prod
+    removeConsole: process.env.NODE_ENV == "production"
+  }
 };
 
 module.exports = nextConfig;
