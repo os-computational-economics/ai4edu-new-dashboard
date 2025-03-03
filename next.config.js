17b61a5d23eb0cb33d1ac20714336d3abc3a9474
Ruihuang Yang <rxy216@case.edu>
2025-03-03 16:28:51 -0500

    feat: Add redirect from maintenance page to root when maintenance mode is off

--- next.config.js
+++ next.config.js
@@ -7,8 +7,13 @@ const nextConfig = {
   },
   redirects() {
     return [
+      // When maintenance mode is on, redirect all non-maintenance pages to maintenance
       process.env.MAINTENANCE_MODE === "1"
         ? { source: "/((?!maintenance).*)", destination: "/maintenance", permanent: false }
+        : null,
+      // When maintenance mode is off, redirect maintenance page to root
+      process.env.MAINTENANCE_MODE !== "1"
+        ? { source: "/maintenance", destination: "/", permanent: false }
         : null,
     ].filter(Boolean);
   },
